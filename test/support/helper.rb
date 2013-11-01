9d7c9733a3998126a4db3786da04951059886aa8
Antonio C Nalesso Moreira <acnalesso@yahoo.co.uk>
2013-11-01 01:37:49 +0000

    explains how support/helper work [ci skip]

--- test/support/helper.rb
+++ test/support/helper.rb
@@ -8,6 +8,11 @@ module Helper
     "The #{msg} is not defined."
   end
 
+  ##
+  # method_name matches anything apart from have
+  # it must be an assertion
+  # if it matches it must be an expectation
+  #
   def or_message(method_name, msg)
     if method_name =! Regexp.new("have")
       notice("assertions " + msg)
@@ -16,10 +21,21 @@ module Helper
     end
   end
 
+  ##
+  # Ask either self or page
+  #
   def respond_to(m)
     self.respond_to?(m) || page.respond_to?(m)
   end
 
+  ##
+  # callee here works but __method__ would not.
+  # that's because callee changes accordingly as
+  # self changes.
+  # id2name simply converts a symbol to a string.
+  # _method_ creates a method to be looked up
+  # _method_ = "refute_method_name"
+  #
   def assert_(method_name)
     callee = send(:__callee__).id2name
     capybara(method_name).each do |assertion|
